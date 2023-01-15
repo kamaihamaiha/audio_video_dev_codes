2839e48f17be22e4bb0709c511fb5c7babdbcc43
kk <kamaihamaiha@gmail.com>
2023-01-15 12:20:23 +0800

    读取图像数据 & 写出码流数据

--- ffmpeg_tutorial/include/io_data.h
+++ ffmpeg_tutorial/include/io_data.h
@@ -8,3 +8,9 @@ extern "C" {
 int32_t open_input_output_files(const char* input_name, const char* output_name);
 
 void close_input_output_files();
+
+// 读取 yuv 数据
+int32_t read_yuv_to_frame(AVFrame *frame);
+
+// 写出码流数据
+void write_pkt_to_file(AVPacket *pkt);
